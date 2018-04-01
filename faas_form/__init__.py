b65c316618c23d49f27829b4acc0284e8efacb20
Unknown <bkehoe@irobot.com>
2018-04-01 15:59:44 -0400

    Add versioning

--- faas_form/__init__.py
+++ faas_form/__init__.py
@@ -1,33 +1,11 @@
-"""
-client:
-{
-  "x-faas-form-request": "schema"
-}
-
-response:
-{
-  "x-faas-form-schema": {
-    "instructions": "foo",
-    "inputs": [
-      {
-        "name": "bar",
-        "type": "string",
-        "pattern": "regex",
-        "default": "value",
-      }
-    ]
-  }
-}
-
-client:
-{
-  "x-faas-form-request": "invoke",
-  "bar": "quux"
-}
-
-"""
-
 from __future__ import absolute_import, print_function
+
+def _get_version():
+    import pkg_resources, codecs
+    if not pkg_resources.resource_exists(__name__, '_version'):
+        return '0.0.0'
+    return codecs.decode(pkg_resources.resource_string(__name__, '_version'),'utf-8').strip()
+__version__ = _get_version()
 
 from faas_form.payloads import is_schema_request, is_invoke_request, set_reinvoke_response, set_result
 from .schema import *
